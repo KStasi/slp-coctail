9702a9defad266853a3592833e49e5cf85b74fbb
kstasi <nastasiakondaurova@gmail.com>
2020-09-07 11:45:00 +0300

    00_createAccount pretified

--- 00_createAccounts.js
+++ 00_createAccounts.js
@@ -1,45 +1,34 @@
 const BITBOXSDK = require("bitbox-sdk");
-const BITBOX = BITBOXSDK.BITBOX;
-const NETWORK = `testnet`;
+const BCHJS = require("@psf/bch-js");
 const fs = require("fs");
-const walletName = process.argv[2] || "account.json";
 
-const bitbox =
-  NETWORK === `mainnet`
-    ? new BITBOX({ restURL: `https://rest.bitcoin.com/v2/` })
-    : new BITBOX({ restURL: `https://trest.bitcoin.com/v2/` });
+(async () => {
+  // configs
+  const NETWORK = `testnet`;
+  const MAINNET_API_FREE = "https://free-main.fullstack.cash/v3/";
+  const TESTNET_API_FREE = "https://free-test.fullstack.cash/v3/";
+  const walletName = process.argv[2] || "account.json";
+  const bchjs = new BCHJS({
+    restURL: NETWORK === "mainnet" ? MAINNET_API_FREE : TESTNET_API_FREE,
+  });
 
-const lang = "english"; // Set the language of the wallet.
+  // generate new account
+  const mnemonic = bchjs.Mnemonic.generate();
+  const rootSeed = await bchjs.Mnemonic.toSeed(mnemonic);
+  const masterHDNode = bchjs.HDNode.fromSeed(rootSeed, NETWORK);
+  const childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/0`);
 
-const outObj = {};
+  // store info
+  const outObj = {
+    mnemonic,
+    cashAddress: bchjs.HDNode.toCashAddress(childNode),
+    slpAddress: bchjs.HDNode.toSLPAddress(childNode),
+    legacyAddress: bchjs.HDNode.toLegacyAddress(childNode),
+    WIF: bchjs.HDNode.toWIF(childNode),
+  };
 
-const mnemonic = bitbox.Mnemonic.generate(
-  128,
-  bitbox.Mnemonic.wordLists()[lang]
-);
-
-outObj.mnemonic = mnemonic;
-
-// root seed buffer
-const rootSeed = bitbox.Mnemonic.toSeed(mnemonic);
-
-// master HDNode
-const masterHDNode = bitbox.HDNode.fromSeed(rootSeed, NETWORK);
-
-// Generate the first 10 seed addresses.
-for (let i = 0; i < 10; i++) {
-  const childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
-
-  // Save the first seed address for use in the .json output file.
-  if (i === 0) {
-    outObj.cashAddress = bitbox.HDNode.toCashAddress(childNode);
-    outObj.legacyAddress = bitbox.HDNode.toLegacyAddress(childNode);
-    outObj.WIF = bitbox.HDNode.toWIF(childNode);
-  }
-}
-
-// Write out the basic information into a json file for other example apps to use.
-fs.writeFile(walletName, JSON.stringify(outObj, null, 2), function (err) {
-  if (err) return console.error(err);
-  console.log(`${walletName} written successfully.`);
-});
+  fs.writeFile(walletName, JSON.stringify(outObj, null, 2), function (err) {
+    if (err) return console.error(err);
+    console.log("wallet.json written successfully.");
+  });
+})();
