626b9cfcb6595c9885f7b77a53f329dc6e7f6f17
kstasi <nastasiakondaurova@gmail.com>
2020-09-07 12:41:36 +0300

    issue token script updated

--- 01_issueToken.js
+++ 01_issueToken.js
@@ -1,173 +1,116 @@
-let BitboxNetwork = require("slpjs").BitboxNetwork;
-const BigNumber = require("bignumber.js");
 const BCHJS = require("@psf/bch-js");
 
+// bchjs configs
 const NETWORK = `testnet`;
-const walletName = process.argv[2] || "account.json";
+const MAINNET_API_FREE = "https://free-main.fullstack.cash/v3/";
+const TESTNET_API_FREE = "https://free-test.fullstack.cash/v3/";
+const bchjs = new BCHJS({
+  restURL: NETWORK === "mainnet" ? MAINNET_API_FREE : TESTNET_API_FREE,
+});
 
+// token configs
 const decimals = 2;
-const name = "TT Token";
-const ticker = "SLPJS";
+const name = "Cocktail token";
+const ticker = "COCK";
 const documentUrl = "info@coctail.io";
 const documentHash = null;
 const initialTokenQty = 1000000;
-const MAINNET_API_FREE = "https://free-main.fullstack.cash/v3/";
-const TESTNET_API_FREE = "https://free-test.fullstack.cash/v3/";
 
-// Instantiate bch-js based on the network.
-let bchjs;
-if (NETWORK === "mainnet") bchjs = new BCHJS({ restURL: MAINNET_API_FREE });
-else bchjs = new BCHJS({ restURL: TESTNET_API_FREE });
-
+// wallet configs
+const walletName = process.argv[2] || "account.json";
 const walletInfo = require("./" + walletName);
 
-async function createToken() {
-  try {
-    const mnemonic = walletInfo.mnemonic;
+(async () => {
+  // account info
+  const rootSeed = await bchjs.Mnemonic.toSeed(walletInfo.mnemonic);
+  const masterHDNode = bchjs.HDNode.fromSeed(rootSeed, NETWORK);
+  const account = masterHDNode.derivePath(`m/44'/145'/0'/0/0`);
+  const cashAddress = bchjs.HDNode.toCashAddress(account);
 
-    // root seed buffer
-    const rootSeed = await bchjs.Mnemonic.toSeed(mnemonic);
-    // master HDNode
-    let masterHDNode;
-    if (NETWORK === "mainnet") masterHDNode = bchjs.HDNode.fromSeed(rootSeed);
-    else masterHDNode = bchjs.HDNode.fromSeed(rootSeed, "testnet"); // Testnet
+  // config fees and dust
+  const txFee = 550;
+  const dustPerOutput = 550;
+  const totalSpent = txFee + 2 * dustPerOutput;
 
-    // HDNode of BIP44 account
-    const account = bchjs.HDNode.derivePath(masterHDNode, "m/44'/145'/0'");
-
-    // m/44'/145'/0'/0/${i}
-    const change = bchjs.HDNode.derivePath(account, "0/0");
-
-    // get the cash address
-    const cashAddress = bchjs.HDNode.toCashAddress(change);
-    // const slpAddress = bchjs.SLP.Address.toSLPAddress(cashAddress)
-
-    // Get a UTXO to pay for the transaction.
-    const data = await bchjs.Electrumx.utxo(cashAddress);
-    const utxos = data.utxos;
-    // console.log(`utxos: ${JSON.stringify(utxos, null, 2)}`)
-
-    if (utxos.length === 0) {
-      console.log(cashAddress);
-      throw new Error("No UTXOs to pay for transaction! Exiting.");
-    }
-
-    // Get the biggest UTXO to pay for the transaction.
-    const utxo = await findBiggestUtxo(utxos);
-    console.log(`utxo: ${JSON.stringify(utxo, null, 2)}`);
-
-    // instance of transaction builder
-    let transactionBuilder;
-    if (NETWORK === "mainnet") {
-      transactionBuilder = new bchjs.TransactionBuilder();
-    } else transactionBuilder = new bchjs.TransactionBuilder("testnet");
-
-    const originalAmount = utxo.value;
-    const vout = utxo.tx_pos;
-    const txid = utxo.tx_hash;
-
-    // add input with txid and index of vout
-    transactionBuilder.addInput(txid, vout);
-
-    // Set the transaction fee. Manually set for ease of example.
-    const txFee = 550;
-
-    // amount to send back to the sending address.
-    // Subtract two dust transactions for minting baton and tokens.
-    const remainder = originalAmount - 546 * 2 - txFee;
-
-    // Generate SLP config object
-    const configObj = {
-      name,
-      ticker,
-      documentUrl,
-      decimals,
-      initialQty: initialTokenQty,
-      documentHash,
-      mintBatonVout: 2,
-    };
-    // Generate the OP_RETURN entry for an SLP GENESIS transaction.
-    const script = bchjs.SLP.TokenType1.generateGenesisOpReturn(configObj);
-    // const data = bchjs.Script.encode(script)
-    // const data = compile(script)
-
-    // OP_RETURN needs to be the first output in the transaction.
-    transactionBuilder.addOutput(script, 0);
-
-    // Send dust transaction representing the tokens.
-    transactionBuilder.addOutput(
-      bchjs.Address.toLegacyAddress(cashAddress),
-      546
-    );
-
-    // Send dust transaction representing minting baton.
-    transactionBuilder.addOutput(
-      bchjs.Address.toLegacyAddress(cashAddress),
-      546
-    );
-
-    // add output to send BCH remainder of UTXO.
-    transactionBuilder.addOutput(cashAddress, remainder);
-
-    // Generate a keypair from the change address.
-    const keyPair = bchjs.HDNode.toKeyPair(change);
-
-    // Sign the transaction with the HD node.
-    let redeemScript;
-    transactionBuilder.sign(
-      0,
-      keyPair,
-      redeemScript,
-      transactionBuilder.hashTypes.SIGHASH_ALL,
-      originalAmount
-    );
-
-    // build tx
-    const tx = transactionBuilder.build();
-    // output rawhex
-    const hex = tx.toHex();
-    console.log(`TX hex: ${hex}`);
-    // console.log(` `)
-
-    // Broadcast transation to the network
-    const txidStr = await bchjs.RawTransactions.sendRawTransaction([hex]);
-    console.log("Check the status of your transaction on this block explorer:");
-    if (NETWORK === "testnet") {
-      console.log(`https://explorer.bitcoin.com/tbch/tx/${txidStr}`);
-    } else console.log(`https://explorer.bitcoin.com/bch/tx/${txidStr}`);
-  } catch (err) {
-    console.error("Error in createToken: ", err);
+  // chose utxo
+  const data = await bchjs.Electrumx.utxo(cashAddress);
+  const utxos = data.utxos;
+  if (utxos.length === 0) {
+    throw new Error("No UTXOs to pay for transaction! Exiting.");
   }
-}
-createToken();
+  const utxo = await findUtxo(utxos, totalSpent);
 
-// Returns the utxo with the biggest balance from an array of utxos.
-async function findBiggestUtxo(utxos) {
-  let largestAmount = 0;
-  let largestIndex = 0;
+  // instance of transaction builder
+  const transactionBuilder = new bchjs.TransactionBuilder(NETWORK);
+  const originalAmount = utxo.value;
+  const vout = utxo.tx_pos;
+  const txid = utxo.tx_hash;
 
-  for (var i = 0; i < utxos.length; i++) {
-    const thisUtxo = utxos[i];
-    // console.log(`thisUTXO: ${JSON.stringify(thisUtxo, null, 2)}`);
+  // add inputs
+  transactionBuilder.addInput(txid, vout);
+  const remainder = originalAmount - totalSpent;
 
-    // Validate the UTXO data with the full node.
-    const txout = await bchjs.Blockchain.getTxOut(
-      thisUtxo.tx_hash,
-      thisUtxo.tx_pos
-    );
-    if (txout === null) {
-      // If the UTXO has already been spent, the full node will respond with null.
-      console.log(
-        "Stale UTXO found. You may need to wait for the indexer to catch up."
-      );
-      continue;
+  // add outputs
+  const configObj = {
+    name,
+    ticker,
+    documentUrl,
+    decimals,
+    initialQty: initialTokenQty,
+    documentHash,
+    mintBatonVout: 2,
+  };
+  const script = bchjs.SLP.TokenType1.generateGenesisOpReturn(configObj);
+  transactionBuilder.addOutput(script, 0);
+  transactionBuilder.addOutput(
+    bchjs.Address.toLegacyAddress(cashAddress),
+    dustPerOutput
+  );
+  transactionBuilder.addOutput(
+    bchjs.Address.toLegacyAddress(cashAddress),
+    dustPerOutput
+  );
+  transactionBuilder.addOutput(cashAddress, remainder);
+
+  // sign
+  const keyPair = bchjs.HDNode.toKeyPair(account);
+  let redeemScript;
+  transactionBuilder.sign(
+    0,
+    keyPair,
+    redeemScript,
+    transactionBuilder.hashTypes.SIGHASH_ALL,
+    originalAmount
+  );
+  const tx = transactionBuilder.build();
+  const hex = tx.toHex();
+  console.log(`Transaction raw hex: `, hex);
+
+  // broadcast
+  const txidStr = await bchjs.RawTransactions.sendRawTransaction([hex]);
+  console.log(`Transaction ID: ${txidStr}`);
+  console.log(
+    `https://explorer.bitcoin.com/${
+      NETWORK == "testnet" ? "tbch" : "bch"
+    }/tx/${txidStr}`
+  );
+})();
+
+async function findUtxo(utxos, amount) {
+  utxos.sort((a, b) => {
+    if (a.value < b.value) {
+      return -1;
     }
+    if (a.value > b.value) {
+      return 1;
+    }
+    return 0;
+  });
 
-    if (thisUtxo.value > largestAmount) {
-      largestAmount = thisUtxo.value;
-      largestIndex = i;
+  for (let i = 0; i < utxos.length; i++) {
+    if (utxos[i].value >= amount) {
+      return utxos[i];
     }
   }
-
-  return utxos[largestIndex];
+  throw new Error(`Wallet does not have a BCH UTXO to pay.`);
 }
